90f1f76a141f26aa751d8b7fa5f96d01af6002f4
Russell <flyingcycling@gmail.com>
2019-09-12 17:41:32 +0800

    - fix：cycle

--- pages/category/components/TimeLineArticleList.js
+++ pages/category/components/TimeLineArticleList.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ArticleItem from './ArticleItem';
 import './TimeLine.less';
 
 function TimeLineArticleList({ id, articles }) {
   const [title, setTitle] = useState('');
-  if (id) {
-    setTitle('某个');
-  } else {
-    setTitle('所有');
-  }
+
+  useEffect(() => {
+    if (id) {
+      setTitle('某个');
+    } else {
+      setTitle('所有');
+    }
+  }, [id]);
+
   return (
     <section className="timeline-list-container">
       <h2>{title}</h2>
-      {articles && articles.length && articles.map(article => <ArticleItem article={article} />)}
+      {articles && articles.map(article => <ArticleItem article={article} />)}
     </section>
   );
 }
