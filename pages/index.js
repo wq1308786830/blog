a50f7c16ef395b45c2fad0976a8cb4dd3231d3bf
Russell <wangqi@tinman.cn>
2021-02-06 23:00:50 +0800

    upgrade

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import css from './Home.less';
+import React from "react";
+import css from "./Home.less";
 
-const FaceLeftPic = '/static/imgs/home/NDk2MDg0NjE1.jpeg';
-const RussellPic = '/static/imgs/home/Bertrand_Russell.jpg';
-const ProgramPic = '/static/imgs/home/language_map.png';
-const FrontendPic = '/static/imgs/home/frontend_map.png';
+const FaceLeftPic = "/static/imgs/home/NDk2MDg0NjE1.jpeg";
+const RussellPic = "/static/imgs/home/Bertrand_Russell.jpg";
+const ProgramPic = "/static/imgs/home/language_map.png";
+const FrontendPic = "/static/imgs/home/frontend_map.png";
 
 export default function Home() {
   return (
@@ -45,6 +45,11 @@ export default function Home() {
           <img src={ProgramPic} alt="后端知识网络" />
         </div>
       </section>
+      <footer>
+        <p>
+          <a href="https://beian.miit.gov.cn" target="_blank">浙ICP备19021274号-2</a>
+        </p>
+      </footer>
     </main>
   );
 }
