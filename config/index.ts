747525f212013c7c1bfd4e7077ba7a8f462b2c59
Russell <russell.wang@ximalaya.com>
2022-11-04 14:14:11 +0800

    chore: 买了个服务器部署了server

--- config/index.ts
+++ config/index.ts
@@ -1,11 +1,10 @@
 export let env = process.env.NODE_ENV;
 
 export const Config = {
-  foreignPrefix: 'http://104.156.250.95:7001',
-  production: 'http://47.112.23.45:5002',
+  production: 'http://81.69.247.116:5002',
   development: 'http://localhost:5002',
 };
 
-env = 'development';
+env = 'production';
 
 export const prefix = Config[env];
