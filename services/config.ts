63216f1e4531a265ce3ceeca6983fd81e68a1e67
Russell <flyingcycling@outlook.com>
2023-12-06 00:52:44 +0800

    feat: 優化代碼

--- services/config.ts
+++ services/config.ts
@@ -1,10 +1,11 @@
-export let env = process.env.NODE_ENV;
+export const env = process.env.NODE_ENV;
 
 export const Config = {
   production: 'http://gin.web-framework-wcof.1681718655914897.cn-hangzhou.fc.devsapp.net',
+  test: 'http://gin.web-framework-wcof.1681718655914897.cn-hangzhou.fc.devsapp.net',
   development: 'http://localhost:5002',
 };
 
-env = 'production';
+// env = 'production';
 
 export const prefix = Config[env];
