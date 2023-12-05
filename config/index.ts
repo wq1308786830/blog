78220cef8e2144f5d17d328c80ac7c667390959f
Russell <flyingcycling@outlook.com>
2023-12-05 16:33:51 +0800

    feat: 更换服务以及pnpm

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 export let env = process.env.NODE_ENV;
 
 export const Config = {
-  production: 'http://81.69.247.116:5002',
+  production: 'http://gin.web-framework-wcof.1681718655914897.cn-hangzhou.fc.devsapp.net',
   development: 'http://localhost:5002',
 };
 
